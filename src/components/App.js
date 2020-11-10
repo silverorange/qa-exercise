f25e7bc139fe1987f834812ab35a600595b15235
Michael Gauthier <mike@silverorange.com>
2020-11-10 17:26:47 -0400

    Convert React app to use modern hooks instead of class components

--- src/components/App.js
+++ src/components/App.js
@@ -1,77 +1,59 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 import Form from './Form';
 
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [items, setItems] = useState([
+    {
+      title: 'Wac, ft. Valeria - “Tortoise”',
+    },
+    {
+      title: 'Kishi Bashi - “The Ballad of Mr Steak”',
+    },
+    {
+      title: 'Lucy Dacus - “Night Shift”',
+    },
+    {
+      title: 'Shamir - “Larry Clark”',
+    },
+    {
+      title: 'Moloko - “Over & Over”',
+    },
+    {
+      title: 'Shugo Tokumaru - “Lita-Ruta”',
+    },
+  ]);
 
-    this.state = {
-      items: [
-        {
-          title: 'Wac, ft. Valeria - “Tortoise”',
-        },
-        {
-          title: 'Kishi Bashi - “The Ballad of Mr Steak”',
-        },
-        {
-          title: 'Lucy Dacus - “Night Shift”',
-        },
-        {
-          title: 'Shamir - “Larry Clark”',
-        },
-        {
-          title: 'Moloko - “Over & Over”',
-        },
-        {
-          title: 'Shugo Tokumaru - “Lita-Ruta”',
-        },
-      ],
-    };
-
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
+  function handleAdd(item) {
+    setItems([...items, item]);
   }
 
-  handleAdd(item) {
-    this.setState((oldState) => ({ items: [...oldState.items, item] }));
-  }
-
-  handleRemove(index) {
-    const {
-      items: { length },
-    } = this.state;
-
-    if (index >= 0 && index <= length - 1) {
-      this.setState((oldState) => {
-        const newItems = oldState.items.slice();
-        newItems.splice(index, 1);
-        return { items: newItems };
-      });
+  function handleRemove(index) {
+    if (index >= 0 && index <= items.length - 1) {
+      const newItems = items.slice();
+      newItems.splice(index, 1);
+      setItems(newItems);
     }
   }
 
-  render() {
-    const { items } = this.state;
-    return (
-      <div className="App">
-        <h1>Items</h1>
-        {items.map((item, index) => (
-          <Item
-            key={index}
-            id={`item_${index}`}
-            title={item.title}
-            onRemove={() => {
-              this.handleRemove(index);
-            }}
-          />
-        ))}
-        <Form onAdd={this.handleAdd} />
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Items</h1>
+      {items.map((item, index) => (
+        <Item
+          key={index}
+          id={`item_${index}`}
+          title={item.title}
+          onRemove={() => {
+            handleRemove(index);
+          }}
+        />
+      ))}
+      <Form onAdd={handleAdd} />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import minLength from '../validations/minLength';
 import maxLength from '../validations/maxLength';
@@ -8,84 +8,67 @@ import './Form.css';
 const MAX_DELAY = 3000;
 const MIN_DELAY = 1000;
 
-class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      loading: false,
-      error: null,
-      title: '',
-    };
+function Form(props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [title, setTitle] = useState('');
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
+  function handleChange(event) {
     const { value } = event.target;
-    this.setState(() => ({ title: value }));
+    setTitle(value);
   }
 
-  handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    const { loading } = this.state;
     if (!loading) {
-      this.setState(() => ({ error: null, loading: true }));
+      setError(null);
+      setLoading(true);
 
       const delay =
         MIN_DELAY + Math.floor(Math.random() * (MAX_DELAY - MIN_DELAY));
 
       // Simulate a network request with timeout.
       setTimeout(() => {
-        const { title } = this.state;
-
         if (!minLength(10, title)) {
-          this.setState(() => ({
-            error: 'Title must be at least 10 characters.',
-            loading: false,
-          }));
+          setError('Title must be at least 10 characters.');
+          setLoading(false);
         } else if (!maxLength(20, title)) {
-          this.setState(() => ({
-            error: 'Title can be at most 20 characters.',
-            loading: false,
-          }));
+          setError('Title can be at most 20 characters.');
+          setLoading(false);
         } else {
-          const { onAdd } = this.props;
-          this.setState(() => ({ title: '', loading: false }));
+          const { onAdd } = props;
+          setTitle('');
+          setLoading(false);
           onAdd({ title });
         }
       }, delay);
     }
   }
 
-  render() {
-    const { loading, error, title } = this.state;
-
-    return (
-      <form className="Form" onSubmit={this.handleSubmit}>
-        {loading && <div className="Form__loading">Loading…</div>}
-        {error && <div className="Form__error">{error}</div>}
-        <label htmlFor="title">
-          <span>
-            Title: <span>(between 10 and 20 characters)</span>
-          </span>
-          <input
-            disabled={loading}
-            id="title"
-            type="text"
-            className="Form__title"
-            value={title}
-            onChange={this.handleChange}
-          />
-        </label>
-        <div className="Form__footer">
-          <button className="Form__add" disabled={loading} type="submit">
-            Add Item
-          </button>
-        </div>
-      </form>
-    );
-  }
+  return (
+    <form className="Form" onSubmit={handleSubmit}>
+      {loading && <div className="Form__loading">Loading…</div>}
+      {error && <div className="Form__error">{error}</div>}
+      <label htmlFor="title">
+        <span>
+          Title: <span>(between 10 and 20 characters)</span>
+        </span>
+        <input
+          disabled={loading}
+          id="title"
+          type="text"
+          className="Form__title"
+          value={title}
+          onChange={handleChange}
+        />
+      </label>
+      <div className="Form__footer">
+        <button className="Form__add" disabled={loading} type="submit">
+          Add Item
+        </button>
+      </div>
+    </form>
+  );
 }
 
 Form.defaultProps = {
